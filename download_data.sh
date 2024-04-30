7afe17fcf609071fdb26e76ad38421f0cc1dedf3
John Hart <jphart@dynamic-oit-ip4-wifirestricted03-10-48-233-233.princeton.edu>
2024-04-30 18:13:00 -0400

    slight bugfix in download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,3 @@
 wget https://huggingface.co/datasets/m-a-p/COIG-CQIA/resolve/main/COIG-CQIA-full.jsonl
-mv COIG-CQIA-full data
+mv COIG-CQIA-full.jsonl data
 echo "Got dataset and moved it to data folder."
