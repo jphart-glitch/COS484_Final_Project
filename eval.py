7cba39563fae89fe044b22f2f491fafb60e1e7b4
John Hart <jphart@dynamic-oit-ip4-wifirestricted03-10-48-233-233.princeton.edu>
2024-05-04 17:31:44 -0400

    new rouge score (using rouge-chinese) pt.4

--- eval.py
+++ eval.py
@@ -105,7 +105,7 @@ def compute_rouge(data):
         for hyp, ref1, ref2 in zip(hypotheses, references1, references2):
             score1 = scorer.get_scores(hyp, ref1)
             score2 = scorer.get_scores(hyp, ref2)
-            best_score = max(score1[0][metrics[0]], score2[0][metrics[0]], key=lambda x: x['f'])
+            best_score = max(score1[0], score2[0], key=lambda x: x['f'])
             for metric in metrics:
                 scores[metric].append(best_score[metric]['f'])
 
