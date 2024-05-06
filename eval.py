15498c3e5495893095304a76badd2dde2ae7eb08
马乐原 <leyuan@dynamic-oit-ip4-wifirestricted03-10-48-164-67.princeton.edu>
2024-05-06 07:55:08 -0400

    debugging

--- eval.py
+++ eval.py
@@ -305,7 +305,7 @@ def compute_claims(data):
     global autoais_model, autoais_tokenizer
     if autoais_model is None:
         logger.info("Loading AutoAIS model...")
-        autoais_model = AutoModelForSeq2SeqLM.from_pretrained(AUTOAIS_MODEL, torch_dtype=torch.bfloat16, max_memory=get_max_memory(), device_map="auto")
+        autoais_model = AutoModelForSeq2SeqLM.from_pretrained(AUTOAIS_MODEL, torch_dtype=torch.bfloat16, max_memory=get_max_memory(), device="cude")
         autoais_tokenizer = AutoTokenizer.from_pretrained(AUTOAIS_MODEL, use_fast=False)
 
     logger.info("Computing claims...")
